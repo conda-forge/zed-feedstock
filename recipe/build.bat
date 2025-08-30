2f4845e0e619b08e3d0bf50d5289df5801600c39
Denizhan Dakılır <denizhandakilirn@gmail.com>
2025-08-30 03:16:59 +0300

    .

--- recipe/build.bat
+++ recipe/build.bat
@@ -21,7 +21,7 @@ if not exist "%SRC_DIR%\.cargo" mkdir "%SRC_DIR%\.cargo"
 copy "%RECIPE_DIR%\config.toml" "%SRC_DIR%\.cargo\config.toml"
 
 REM Enable proper Spectre mitigations with /Qspectre, zed uses spectre mitigations so better to compile all their code like this
-set CL=/Qspectre /MD %CL%
+set CL=/Qspectre %CL%
 
 REM Fix ssh2 library name mismatch - Rust expects ssh2.lib but conda-forge provides libssh2.lib
 copy "%LIBRARY_LIB%\libssh2.lib" "%LIBRARY_LIB%\ssh2.lib"
